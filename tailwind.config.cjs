e46c171791553bfd1ea3e50e47ddab7360ac8b40
andleo17 <andres.gastulo@gmail.com>
2023-10-01 17:21:11 -0500

    fix fonts and implementing download button

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				gothic: ['"Bold Ayres"', '"Segoe UI"'],
+				paragraph: ['Kievit Serif', '"Segoe UI"'],
+				title: ['"Bodoni Elegant"', '"Segoe UI"'],
+			},
+		},
 	},
 	plugins: [],
-}
+};
